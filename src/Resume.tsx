e735104ac7810ce51c28a66773f53587d56ae569
Valentin <valentin.vignal@gmail.com>
2017-12-27 16:22:32 +0100

    Ajout de l'affiche de l'historique dans Resume

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -12,10 +12,10 @@ const afficheTransactions_Resume = (tableau: Transaction[]) => {
     let tab_retour:any = [];
     for (var k :number=0; k< tableau.length && (nb<10); k++ ){
         if (tableau[k].validated){
-            tab_retour.push(
+            tab_retour.unshift(
                 <tr key={k}>
                     <td key={"id"}>
-                        Transaction n°{tableau[k].id}
+                        Transaction n°{tableau[k].id +1}
                     </td>
                     <td key={'montant'}>
                         {tableau[k].montant}€
@@ -25,6 +25,19 @@ const afficheTransactions_Resume = (tableau: Transaction[]) => {
         }
     }
     return tab_retour
+};
+
+const afficheHistorique = (tableau : number[]) => {
+    let nb:number = 0;
+    let tab_retour:any = [];
+    for (var k :number=0; k< tableau.length && (nb<10); k++ ) {
+        tab_retour.push(
+            <td key={k}>
+                {tableau[k]}€
+            </td>
+        )
+    }
+    return tab_retour;
 };
 
 
@@ -37,6 +50,13 @@ export class Resume extends React.Component <Props, State> {
                     {this.props.banque.argent}
                     €
                 </h1>
+                <table>
+                    <tbody>
+                        <tr>
+                            {...afficheHistorique(this.props.banque.historique)}
+                        </tr>
+                    </tbody>
+                </table>
                 <table>
                     <tbody>
                     {...afficheTransactions_Resume(this.props.banque.transactions)}
